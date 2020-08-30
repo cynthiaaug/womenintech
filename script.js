01a419ffe6c6b3e99c808045aa25bec175385101
cynthiaaug <violetcynthia.augustine@gmail.com>
2020-08-30 12:38:11 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function newStyle() {
+  let newColor = '';
+  let newFont = '';
+  let x = Math.floor(Math.random()*3);
+  switch (x){
+    case 0:
+      newColor = 'red';
+      newFont = 'Times New Roman';
+      break;
+    case 1:
+      newColor = 'green';
+      newFont = 'Verdana';
+      break;
+    case 2:
+      newColor = 'blue';
+      newFont = 'Helvetica';
+      break;
+  }
+  var elem = document.getElementById('logo')
+  elem.style.color = newColor;
+  elem.style.fontFamily = newFont;
+}
